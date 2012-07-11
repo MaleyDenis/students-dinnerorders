8a6c2843b13bd5076bd249817c3a7be00f3c0fff
fry1228@gmail.com <fry1228@gmail.com@c07ae454-4001-a9b2-41ff-d98365e2add6>
2012-07-11 14:18:36 +0000

    [Issue 3] new entity order was created

---
+++ trunk/core/src/main/java/com/exadel/dinnerorders/entity/Order.java
@@ -0,0 +1,52 @@
+package com.exadel.dinnerorders.entity;
+
+import java.util.Date;
+import java.util.List;
+
+
+public class Order {
+    private long id;
+    private long userID;
+    private double cost;
+    private Date datePayment;
+    private Date dateOrder;
+    private List<MenuItem> menuItemList;
+    private Order(long id, long userID, double cost, Date dateOrder, Date datePayment){
+        this.id = id;
+        this.userID = userID;
+        this.cost = cost;
+        this.dateOrder = dateOrder;
+        this.datePayment = datePayment;
+    }
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public Date getDatePayment() {
+        return datePayment;
+    }
+
+    public void setDatePayment(Date datePayment) {
+        this.datePayment = datePayment;
+    }
+
+    public Date getDateOrder() {
+        return dateOrder;
+    }
+
+    public void setDateOrder(Date dateOrder) {
+        this.dateOrder = dateOrder;
+    }
+
+    public long getUserID() {
+        return userID;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
