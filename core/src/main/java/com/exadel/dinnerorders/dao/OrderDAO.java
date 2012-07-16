fccb6dbb0d7e254820b08e8a2fc6e7750679ec22
fry1228 <fry1228@gmail.com>
2012-07-16 09:23:38 +0000

    [Issue 7] fixed OrderDAO.

--- core/src/main/java/com/exadel/dinnerorders/dao/OrderDAO.java
+++ core/src/main/java/com/exadel/dinnerorders/dao/OrderDAO.java
@@ -2,20 +2,18 @@ package com.exadel.dinnerorders.dao;
 
 import com.exadel.dinnerorders.entity.Order;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
 import java.sql.SQLException;
 
-/**
- * User: Asus
- * */
+
 public class OrderDAO extends BaseDAO<Order> {
+
     public boolean create(Order newItem)  {
         Connection connection = connection();
         try {
             if (connection != null){
-                PreparedStatement pst = connection.prepareStatement("INSERT INTO orderv15 VALUES(?,?,?,?,?)");
+                PreparedStatement pst = (PreparedStatement)connection.prepareStatement("INSERT INTO orderv15 VALUES(?,?,?,?,?)");
                 pst.setLong(1,newItem.getId());
                 pst.setLong(2,newItem.getUserID());
                 pst.setDouble(3, newItem.getCost());
@@ -23,35 +21,39 @@ public class OrderDAO extends BaseDAO<Order> {
                 pst.setTimestamp(5,new java.sql.Timestamp(newItem.getDatePayment().getTime()));
                 pst.executeUpdate();
                 pst.close();
+                disconnect(connection);
                 return true;
             }
-            disconnect(connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return false;
     }
+
     public boolean update(Order item) {
         return false;
     }
+
     public boolean delete(Order item) {
         Connection connection = connection();
         try {
             if (connection != null){
-                PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM orderv15 WHERE id=?");
+                PreparedStatement preparedStatement = (PreparedStatement)connection.prepareStatement("DELETE FROM orderv15 WHERE id=?");
                 preparedStatement.setLong(1,item.getId());
                 preparedStatement.executeUpdate();
                 preparedStatement.close();
+                disconnect(connection);
                 return true;
             }
-            disconnect(connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return false;
     }
+
     public Order read() {
         Connection connection = connection();
+
         return null;
     }
 }
