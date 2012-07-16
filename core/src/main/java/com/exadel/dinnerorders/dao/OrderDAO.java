5df55a40b67dd1c4418cce09ec339efe4a489e30
fry1228 <fry1228@gmail.com>
2012-07-16 14:56:22 +0000

    [Issue 16, Issue 8] Delete Order item and Create test delete and Create service class for update 'order' object .

--- core/src/main/java/com/exadel/dinnerorders/dao/OrderDAO.java
+++ core/src/main/java/com/exadel/dinnerorders/dao/OrderDAO.java
@@ -4,7 +4,12 @@ import com.exadel.dinnerorders.entity.Order;
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.Statement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 
 public class OrderDAO extends BaseDAO<Order> {
@@ -13,7 +18,8 @@ public class OrderDAO extends BaseDAO<Order> {
         Connection connection = connection();
         try {
             if (connection != null){
-                PreparedStatement pst = (PreparedStatement)connection.prepareStatement("INSERT INTO orderv15 VALUES(?,?,?,?,?)");
+                PreparedStatement pst = (PreparedStatement)connection.prepareStatement
+                        ("INSERT INTO orderv15 VALUES(?,?,?,?,?)");
                 pst.setLong(1,newItem.getId());
                 pst.setLong(2,newItem.getUserID());
                 pst.setDouble(3, newItem.getCost());
@@ -31,6 +37,27 @@ public class OrderDAO extends BaseDAO<Order> {
     }
 
     public boolean update(Order item) {
+        Connection connection = connection();
+        Double cost;
+        Date datePayment;
+        try {
+            if (connection != null){
+                cost = item.getCost();
+                datePayment = item.getDatePayment();
+                PreparedStatement preparedStatement = (PreparedStatement)connection.prepareStatement
+                        ("UPDATE orderv15 SET" +" cost = ?,datePayment = ? ");
+                preparedStatement.setDouble(1,cost);
+                preparedStatement.setTimestamp(2,new java.sql.Timestamp(datePayment.getTime()));
+                preparedStatement.executeUpdate();
+                preparedStatement.close();
+                disconnect(connection);
+                return true;
+
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
@@ -38,7 +65,8 @@ public class OrderDAO extends BaseDAO<Order> {
         Connection connection = connection();
         try {
             if (connection != null){
-                PreparedStatement preparedStatement = (PreparedStatement)connection.prepareStatement("DELETE FROM orderv15 WHERE id=?");
+                PreparedStatement preparedStatement = (PreparedStatement)connection.prepareStatement
+                        ("DELETE FROM orderv15 WHERE id=?");
                 preparedStatement.setLong(1,item.getId());
                 preparedStatement.executeUpdate();
                 preparedStatement.close();
@@ -52,8 +80,26 @@ public class OrderDAO extends BaseDAO<Order> {
     }
 
     public Order read() {
-        Connection connection = connection();
-
-        return null;
+        return  null;
     }
+    public List<Order> readL(){
+        Order order;
+        List<Order> orders = new ArrayList<Order>();
+
+        Connection connection = connection();
+        try {
+            Statement statement = (Statement)connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM orderv15");
+            while (resultSet.next()){
+                order = new Order(resultSet.getLong("id"),resultSet.getLong("userID"),resultSet.getDouble("cost"),
+                        resultSet.getDate("dateOrder"),resultSet.getDate("datePayment"));
+                orders.add(order);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return orders;
+
+    }
+
 }
--- core/src/test/java/com/exadel/dinnerorders/dao/OrderTest.java
+++ core/src/test/java/com/exadel/dinnerorders/dao/OrderTest.java
@@ -3,50 +3,69 @@ package com.exadel.dinnerorders.dao;
 import com.exadel.dinnerorders.entity.Order;
 import junit.framework.Assert;
 import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Date;
 
 /**
- * Created with IntelliJ IDEA.
- * User: Asus
- * Date: 16.07.12
- * Time: 1:47
- * To change this template use File | Settings | File Templates.
- */
-public class OrderTest extends TestCase {
-    @Test
+ * * User: Denis
+ * */
+
+ public class OrderTest extends TestCase {
+    @Before
     public void setUp() throws Exception {
 
     }
-
+    @After
     public void tearDown() throws Exception {
 
     }
 
-    public void testCreate() throws Exception {
-
-        OrderDAO orderDAO = new OrderDAO();
-        Date date = new Date();
-        Order order = new Order(new Long(1),new Long(1),23.5,date,date);
-        Assert.assertTrue(!order.equals(null));
-        assertEquals(order,order);
-        assertTrue(orderDAO.create(order));
-        Order order1 = new Order(new Long(2),new Long(2),23.5,date,date);
-        Assert.assertTrue (!order1.equals(null));;
-        assertTrue(orderDAO.create(order1));
-        Order order2 = new Order(new Long(3),new Long(3),23.5,date,date);
-        Assert.assertTrue (!order2.equals(null));
-        assertTrue(orderDAO.create(order2));
+    private void initialization(){
 
     }
+    @Test
+    public void testCreate() throws Exception {
+        OrderDAO orderDAO = new OrderDAO();
+        Date date = new Date();
+        Order order = new Order((long)1,(long)1,23.5,date,date);
+        Assert.assertNotNull(order);
+        //assertEquals(order, order);
+        assertTrue(orderDAO.create(order));
+
+        Order order1 = new Order((long)2,(long)2,23.5,date,date);
+        Assert.assertNotNull(order1);
+        assertTrue(orderDAO.create(order1));
+
+        Order order2 = new Order((long)3,(long)3,23.5,date,date);
+        Assert.assertNotNull(order2);
+        assertTrue(orderDAO.create(order2));
+    }
+
 
     public void testUpdate() throws Exception {
-
+        OrderDAO orderDAO = new OrderDAO();
+        Date date = new Date();
+        Order order2 = new Order((long)4,(long)3,23.5,date,date);
+        orderDAO.create(order2);
+        order2.setCost(54645);
+        order2.setDatePayment(date);
+        orderDAO.update(order2);
     }
 
     public void testDelete() throws Exception {
-
+        OrderDAO orderDAO = new OrderDAO();
+        Date date = new Date();
+        Order order = new Order((long)1,(long)2,645.65,date,date);
+        assertTrue(orderDAO.delete(order));
+        Order order1 = new Order((long)2,(long)2,555,date,date);
+        assertTrue(orderDAO.delete(order1));
+        Order order2 = new Order((long)3,(long)2,645.65,date,date);
+        assertTrue(orderDAO.delete(order2));
+        Order order3 = new Order((long)4,(long)2,645.65,date,date);
+        assertTrue(orderDAO.delete(order3));
     }
 
     public void testRead() throws Exception {
