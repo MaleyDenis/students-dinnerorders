7ac646dc87f6b830584d22628bcbd19e8c04d8cc
123dudar@gmail.com <123dudar@gmail.com@c07ae454-4001-a9b2-41ff-d98365e2add6>
2012-07-17 13:49:40 +0000

    [.....] scl script with 3 data base: menus, menuitems, menu_menuitem.

--- trunk/core/db/script.sql
+++ trunk/core/db/script.sql
@@ -10,3 +10,29 @@ PRIMARY KEY (id)
 )
 ENGINE=InnoDB;
 
+CREATE TABLE IF NOT EXISTS dinnerorders.menus (
+id  bigint(20) NOT NULL ,
+cafename varchar(45) NOT NULL ,
+datestart timestamp NOT NULL ,
+dateend timestamp NOT NULL ,
+PRIMARY KEY (id)
+)
+ENGINE=InnoDB,
+COLLATE=utf8;
+
+CREATE TABLE IF NOT EXISTS dinnerorders.menuitems (
+id  bigint(20) NOT NULL ,
+weekday enum('MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY') NOT NULL ,
+description varchar(45) NOT NULL ,
+cost double NOT NULL ,
+PRIMARY KEY (id)
+)
+ENGINE=InnoDB,
+COLLATE=utf8;
+
+CREATE TABLE IF NOT EXISTS dinnerorders.menu_menuitem (
+idmenu  bigint(20) NOT NULL ,
+idmenuitem bigint(20) NOT NULL
+)
+ENGINE=InnoDB,
+COLLATE=utf8;
