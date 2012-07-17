34191fb553f5babb690132a4ebc337cec6d46f87
dimashulgin2012 <dimashulgin2012@gmail.com>
2012-07-17 21:54:44 +0000

    [Issue 29] Done. Add User class and DAO  for save/load user to/from db.

---
+++ core/src/main/java/com/exadel/dinnerorders/entity/User.java
@@ -0,0 +1,57 @@
+package com.exadel.dinnerorders.entity;
+
+/**
+ * User: Dima Shulgin
+ * Date: 17.07.12
+ */
+public class User {
+
+    private int id;
+    private String ldapLogin;
+    private String userName;
+
+
+    public User(int id, String userName, String ldapLogin) {
+        this.id = id;
+        this.userName = userName;
+        this.ldapLogin = ldapLogin;
+    }
+
+
+    public User(User newUser) {
+        this.id = newUser.getId();
+        this.userName = newUser.getUserName();
+        this.ldapLogin = newUser.getLdapLogin();
+    }
+
+    public User() {
+
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLdapLogin() {
+        return ldapLogin;
+    }
+
+    public void setLdapLogin(String ldapLogin) {
+        this.ldapLogin = ldapLogin;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+}
