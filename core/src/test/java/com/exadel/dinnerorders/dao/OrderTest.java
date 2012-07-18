5fa9c892697da8769c0f27454239885d11a4a15c
fry1228 <fry1228@gmail.com>
2012-07-18 13:31:25 +0000

    [Issue 16] fixed bugs.

--- core/src/test/java/com/exadel/dinnerorders/dao/OrderTest.java
+++ core/src/test/java/com/exadel/dinnerorders/dao/OrderTest.java
@@ -1,19 +1,18 @@
 package com.exadel.dinnerorders.dao;
 
+import com.exadel.dinnerorders.entity.MenuItem;
 import com.exadel.dinnerorders.entity.Order;
+import com.exadel.dinnerorders.entity.Weekday;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
-/**
- * * User: Denis
- * */
 
  public class OrderTest extends TestCase {
     @Before
@@ -30,26 +29,37 @@ import java.util.Date;
     public void testCreate() throws Exception {
         OrderDAO orderDAO = new OrderDAO();
         Date date = new Date();
+
         Order order = new Order((long)1,(long)1,23.5,date,date);
         Assert.assertNotNull(order);
-        //assertEquals(order, order);
+
+        MenuItem  menuItem1 = new MenuItem((long)2, Weekday.MONDAY, "4", (double)1);
+        MenuItemDAO menuItemDAO = new MenuItemDAO();
+        assertTrue(menuItemDAO.create(menuItem1));
+        List<MenuItem> menuItems = new ArrayList<MenuItem>();
+        menuItems.add(menuItem1);
+        order.setMenuItemList(menuItems);
+
         assertTrue(orderDAO.create(order));
 
         Order order1 = new Order((long)2,(long)2,23.5,date,date);
         Assert.assertNotNull(order1);
-        assertTrue(orderDAO.create(order1));
 
-        Order order2 = new Order((long)3,(long)3,23.5,date,date);
-        Assert.assertNotNull(order2);
-        assertTrue(orderDAO.create(order2));
+        MenuItem  menuItem2 = new MenuItem((long)3, Weekday.MONDAY, "4", (double)1);
+        menuItemDAO.create(menuItem2);
+        List<MenuItem> menuItems2 = new ArrayList<MenuItem>();
+        menuItems.add(menuItem2);
+        order1.setMenuItemList(menuItems2);
+        orderDAO.create(order1);
+
+
     }
 
-    @Test
+   @Test
     public void testUpdate() throws Exception {
         OrderDAO orderDAO = new OrderDAO();
         Date date = new Date();
-        Order order2 = new Order((long)4,(long)3,23.5,date,date);
-        orderDAO.create(order2);
+        Order order2 = new Order((long)2,(long)3,23.5,date,date);
         order2.setCost(54645);
         order2.setDatePayment(date);
         orderDAO.update(order2);
@@ -59,31 +69,32 @@ import java.util.Date;
     public void  testLoadAll() throws Exception {
         OrderDAO orderDAO = new OrderDAO();
         Collection<Order> collection = orderDAO.loadAll();
+        double arr[] = {54645, 23.5};
+        int i = 0;
         for (Order order : collection) {
-            System.out.println(order.getCost());
+            assertTrue(order.getCost() == arr[i]);
+            i++;
         }
-
     }
-
-    @Test
-    public void testDelete() throws Exception {
-        OrderDAO orderDAO = new OrderDAO();
-        Date date = new Date();
-        Order order = new Order((long)1,(long)2,645.65,date,date);
-        assertTrue(orderDAO.delete(order));
-        Order order1 = new Order((long)2,(long)2,555,date,date);
-        assertTrue(orderDAO.delete(order1));
-        Order order2 = new Order((long)3,(long)2,645.65,date,date);
-        assertTrue(orderDAO.delete(order2));
-        Order order3 = new Order((long)4,(long)2,645.65,date,date);
-        assertTrue(orderDAO.delete(order3));
-    }
-
     @Test
     public void testLoad() throws Exception {
         OrderDAO orderDAO = new OrderDAO();
-        System.out.println(orderDAO.load((long)6).getCost());
+        assertTrue(orderDAO.load((long)2).getCost()==54645);
     }
 
+   @Test
+    public void testDelete() throws Exception {
+        OrderDAO orderDAO = new OrderDAO();
+        MenuItemDAO menuItemDAO = new MenuItemDAO();
+        Date date = new Date();
+        Order order = new Order((long)1,(long)2,645.65,date,date);
+        assertTrue(orderDAO.delete(order));
+        MenuItem  menuItem1 = new MenuItem((long)2, Weekday.MONDAY, "4", (double)1);
+        assertTrue(menuItemDAO.delete(menuItem1));
+        Order order1 = new Order((long)2,(long)2,555,date,date);
+        assertTrue(orderDAO.delete(order1));
+        MenuItem  menuItem2 = new MenuItem((long)3, Weekday.MONDAY, "4", (double)1);
+        assertTrue(menuItemDAO.delete(menuItem2));
+    }
 }
 
