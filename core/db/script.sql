d709353671afc5ce9ed8439889a26b1a417b5db8
alexander.okunevich <alexander.okunevich@c07ae454-4001-a9b2-41ff-d98365e2add6>
2012-07-18 06:20:52 +0000

    [misc] fix tables

--- core/db/script.sql
+++ core/db/script.sql
@@ -1,46 +1,45 @@
 CREATE DATABASE IF NOT EXISTS dinnerorders;
 
 CREATE TABLE IF NOT EXISTS dinnerorders.order (
-order_id  bigint(20) NOT NULL ,
-user_id  bigint(20) NOT NULL ,
-cost double NOT NULL ,
-date_payment  timestamp NULL DEFAULT NULL ,
-date_order  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP ,
-PRIMARY KEY (id)
+order_id  bigint(20) NOT NULL,
+user_id  bigint(20) NOT NULL,
+cost double NOT NULL,
+date_payment  timestamp NULL DEFAULT NULL,
+date_order  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (order_id)
 )
 ENGINE=InnoDB;
-COLLATE=utf8;
 
 CREATE TABLE IF NOT EXISTS  dinnerorders.order_menuitem(
-order_id bigint(20) NOT  NULL;
-munu_item_id bigint(20) NOT NULL;
+id bigint(20) NOT  NULL,
+order_id bigint(20) NOT  NULL,
+menu_item_id bigint(20) NOT NULL,
+PRIMARY KEY (id)
 )
 ENGINE=InnoDB;
-COLLATE=utf8;
 
-CREATE TABLE IF NOT EXISTS dinnerorders.menus (
-id  bigint(20) NOT NULL ,
-cafename varchar(45) NOT NULL ,
-datestart timestamp NOT NULL ,
-dateend timestamp NOT NULL ,
-PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS dinnerorders.menu (
+menu_id  bigint(20) NOT NULL,
+cafename varchar(45) NOT NULL,
+date_start timestamp NOT NULL,
+date_end timestamp NOT NULL,
+PRIMARY KEY (menu_id)
 )
-ENGINE=InnoDB,
-COLLATE=utf8;
+ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS dinnerorders.menuitems (
-id  bigint(20) NOT NULL ,
-weekday enum('MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY') NOT NULL ,
-description varchar(45) NOT NULL ,
-cost double NOT NULL ,
-PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS dinnerorders.menuitem (
+menuitem_id bigint(20) NOT NULL,
+weekday varchar(10) NOT NULL,
+description varchar(100) NOT NULL,
+cost double NOT NULL,
+PRIMARY KEY (menuitem_id)
 )
-ENGINE=InnoDB,
-COLLATE=utf8;
+ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS dinnerorders.menu_menuitem (
-idmenu  bigint(20) NOT NULL ,
-idmenuitem bigint(20) NOT NULL
+id bigint(20) NOT  NULL,
+menu_id  bigint(20) NOT NULL ,
+menuitem_id bigint(20) NOT NULL,
+PRIMARY KEY (id)
 )
-ENGINE=InnoDB,
-COLLATE=utf8;
+ENGINE=InnoDB;
