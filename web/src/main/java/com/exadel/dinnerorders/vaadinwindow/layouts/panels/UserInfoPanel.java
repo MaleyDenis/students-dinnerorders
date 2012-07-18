ee5d7a812e935b56ba1fefae682b29b23a215bcf
snalivko93 <snalivko93@gmail.com>
2012-07-18 11:12:54 +0000

    [Issue 23] refactoring.

--- web/src/main/java/com/exadel/dinnerorders/vaadinwindow/layouts/panels/UserInfoPanel.java
+++ web/src/main/java/com/exadel/dinnerorders/vaadinwindow/layouts/panels/UserInfoPanel.java
@@ -1,46 +1,57 @@
 package com.exadel.dinnerorders.vaadinwindow.layouts.panels;
 
 import com.exadel.dinnerorders.vaadinwindow.application.Application;
+import com.exadel.dinnerorders.vaadinwindow.events.AuthenticationEvent;
 import com.exadel.dinnerorders.vaadinwindow.events.SignOutEvent;
+import com.google.common.eventbus.Subscribe;
 import com.vaadin.terminal.ExternalResource;
 import com.vaadin.ui.*;
 
+import java.util.Scanner;
+
 public class UserInfoPanel extends Panel {
-    public static final int DEFAULT_LAYOUT_COLUMN_COUNT = 20;
-    public static final int DEFAULT_LAYOUT_ROW_COUNT = 20;
+    public static final int DEFAULT_LAYOUT_COLUMN_COUNT = 3;
+    public static final int DEFAULT_LAYOUT_ROW_COUNT = 3;
     private Label nameLabel;
     private Label lastNameLabel;
     private Label nameCaption;
     private Label lastNameCaption;
     private Button signOutButton;
-    private String userName;
-    private String userLastName;
     private GridLayout layout;
+    private Embedded userPhoto;
 
     public UserInfoPanel() {
         super();
+        Application.getInstance().getEventBus().register(this);
+        setDefaultUserInfo();
         initComponents();
         initLayout();
         locateComponents();
     }
 
+    private void setDefaultUserInfo() {
+        userPhoto = new Embedded("", new ExternalResource("/VAADIN/themes/runo/icons/64/user.png"));
+    }
+
     private void locateComponents() {
-        layout.addComponent(new Embedded("", new ExternalResource("/VAADIN/themes/runo/icons/64/user.png")), 1, 1, 4, 9);
-        layout.addComponent(nameCaption,  5, 2, 10, 2);
-        layout.addComponent(lastNameCaption,  5, 6, 10, 6);
-        layout.addComponent(nameLabel,  11, 2, 19, 2);
-        layout.addComponent(lastNameLabel,  11, 6, 19, 6);
-        layout.addComponent(signOutButton, 5, 8, 19, 18);
+        layout.addComponent(userPhoto, 0, 0, 0, 2);
+        layout.addComponent(nameCaption,  1, 0);
+        layout.addComponent(lastNameCaption,  1, 1);
+        layout.addComponent(nameLabel,  2, 0);
+        layout.addComponent(lastNameLabel, 2, 1);
+        layout.addComponent(signOutButton, 1, 2);
     }
 
     private void initLayout() {
         layout = new GridLayout(DEFAULT_LAYOUT_COLUMN_COUNT, DEFAULT_LAYOUT_ROW_COUNT);
         layout.setWidth(100, UNITS_PERCENTAGE);
+        layout.setColumnExpandRatio(0, 0.15f);
+        layout.setColumnExpandRatio(1, 0.35f);
+        layout.setColumnExpandRatio(2, 0.4f);
         setContent(layout);
     }
 
     private void initComponents() {
-        getUserInfo();
         initButton();
         initLabels();
     }
@@ -48,8 +59,8 @@ public class UserInfoPanel extends Panel {
     private void initLabels() {
         nameCaption = new Label("Name");
         lastNameCaption = new Label("LastName");
-        nameLabel = new Label(userName);
-        lastNameLabel = new Label(userLastName);
+        nameLabel = new Label("Name");
+        lastNameLabel = new Label("Last name");
     }
 
     private void initButton() {
@@ -59,12 +70,16 @@ public class UserInfoPanel extends Panel {
             @Override
             public void buttonClick(Button.ClickEvent clickEvent) {
                 Application.getInstance().getEventBus().post(new SignOutEvent());
+                Application.getInstance().setUserName(null);
             }
         });
     }
 
-    private void getUserInfo() {
-        userName = "userName";
-        userLastName = "userLastName";
+    @Subscribe
+    public void authenticationPassed(AuthenticationEvent aEvent) {
+        Scanner scanner = new Scanner(Application.getInstance().getUserName());
+        lastNameLabel.setValue(scanner.next());
+        nameLabel.setValue(scanner.next());
+        //TODO:load user photo
     }
 }
