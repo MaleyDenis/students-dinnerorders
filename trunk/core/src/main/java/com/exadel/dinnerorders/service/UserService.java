826abf75481b8a98a73b2586999a45fc81f97df8
snalivko93@gmail.com <snalivko93@gmail.com@c07ae454-4001-a9b2-41ff-d98365e2add6>
2012-07-19 09:28:58 +0000

    [Issue 31] loadAllUserFromLdap method was added to com.exadel.dinnerorders.service UserService class. In properties files was added information for LdapService class

--- trunk/core/src/main/java/com/exadel/dinnerorders/service/UserService.java
+++ trunk/core/src/main/java/com/exadel/dinnerorders/service/UserService.java
@@ -1,6 +1,7 @@
 package com.exadel.dinnerorders.service;
 
 import com.exadel.dinnerorders.dao.UserDAO;
+import com.exadel.dinnerorders.entity.Role;
 import com.exadel.dinnerorders.entity.User;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
@@ -8,11 +9,13 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * User: Dima Shulgin
  * Date: 19.07.12
  */
+
 public class UserService {
     private static Logger logger = Logger.getLogger(UserService.class);
 
@@ -22,6 +25,20 @@ public class UserService {
         return (ArrayList<User>) userDAO.loadAll();
     }
 
+    public static Collection<User> loadAllUsersFromLdap() {
+        Collection<String> userNames = new LdapService().loadAll();
+        Collection<User> users = new ArrayList<User>();
+        for (String userName : userNames) {
+            User user = findUserbyUserName(userName);
+            if (user == null) {
+                user = new User();
+                user.setId(null);
+                user.setRole(Role.USER);
+            }
+            users.add(user);
+        }
+        return users;
+    }
 
     public static User findUserbyUserName(final String username) {
 
@@ -32,7 +49,6 @@ public class UserService {
                 return StringUtils.equals(u.getUserName(), username);
             }
         });
-
 
         if (iterables.iterator().hasNext()) {
             return iterables.iterator().next();
