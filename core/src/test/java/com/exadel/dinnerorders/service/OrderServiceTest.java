b52bd396a69d84dee063f358881633df328ef182
fry1228 <fry1228@gmail.com>
2012-07-19 11:08:29 +0000

    [Issue 16] corrected warning.

--- core/src/test/java/com/exadel/dinnerorders/service/OrderServiceTest.java
+++ core/src/test/java/com/exadel/dinnerorders/service/OrderServiceTest.java
@@ -2,14 +2,13 @@ package com.exadel.dinnerorders.service;
 
 import com.exadel.dinnerorders.entity.Order;
 import junit.framework.TestCase;
-import org.junit.Test;
+
 
 import java.util.Collection;
 
 
 
 public class OrderServiceTest extends TestCase {
-    @Test
     public void testSortedALL() throws Exception {
 
         Collection<Order> collection = OrderService.sortedAll();
