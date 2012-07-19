b055bf91b2d999f0c6174778822bea3ab585a6e5
fry1228 <fry1228@gmail.com>
2012-07-19 10:44:11 +0000

    [Issue 16] added sort by date order fixed OrderDAO, create OrderServiceTest .

--- core/src/main/java/com/exadel/dinnerorders/service/OrderService.java
+++ core/src/main/java/com/exadel/dinnerorders/service/OrderService.java
@@ -1,22 +1,33 @@
 package com.exadel.dinnerorders.service;
 
+import com.exadel.dinnerorders.dao.OrderDAO;
 import com.exadel.dinnerorders.entity.Order;
-
 import com.google.common.collect.Ordering;
+
+
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 
 
 public class OrderService {
-    Comparator<Order> byDate = new Comparator<Order>() {
+    public static  Comparator<Order> byDate = new Comparator<Order>() {
         public int compare(final Order p1, final Order p2) {
             return p1.getDateOrder().compareTo(p2.getDateOrder());
         }
     };
-    public List<Order> sorted(List<Order> orders){
-        Ordering<Order>orderOrdering = Ordering.from(byDate);
+    public static Collection<Order> sorted(Collection<Order> orders){
+        Ordering<Order> orderOrdering = Ordering.from(byDate);
         List<Order> orderList = orderOrdering.reverse().sortedCopy(orders);
         return orderList;
+    }
+    public static Collection<Order>sortedAll(){
+        OrderDAO orderDAO = new OrderDAO();
+        Collection<Order> ordersSorted = orderDAO.loadAll();
+        if (ordersSorted !=null){
+            sorted(ordersSorted);
+        }
+        return ordersSorted;
     }
 }
 
