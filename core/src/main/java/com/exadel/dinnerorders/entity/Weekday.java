1228e0c645725f75b7720560f16203180f83809e
123dudar <123dudar@gmail.com>
2012-07-20 13:16:37 +0000

    [MISC] fixed class Weekday(new method getWeekday). new class CreateMenuPanel(does not work).

--- core/src/main/java/com/exadel/dinnerorders/entity/Weekday.java
+++ core/src/main/java/com/exadel/dinnerorders/entity/Weekday.java
@@ -13,4 +13,31 @@ public enum Weekday {
     FRIDAY,
     SATURDAY,
     SUNDAY;
+
+    public static Weekday getWeekday(int i){
+        switch(i){
+            case 1:{
+                return Weekday.MONDAY;
+            }
+            case 2:{
+                return Weekday.TUESDAY;
+            }
+            case 3:{
+                return Weekday.WEDNESDAY;
+            }
+            case 4:{
+                return Weekday.THURSDAY;
+            }
+            case 5:{
+                return Weekday.FRIDAY;
+            }
+            case 6:{
+                return Weekday.SATURDAY;
+            }
+            case 7:{
+                return Weekday.SUNDAY;
+            }
+            default: return null;
+        }
+    }
 }
