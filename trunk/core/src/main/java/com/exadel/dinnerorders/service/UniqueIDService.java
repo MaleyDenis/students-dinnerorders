cf68435593ad740f7a32bd5de98ba7ede39377d7
snalivko93@gmail.com <snalivko93@gmail.com@c07ae454-4001-a9b2-41ff-d98365e2add6>
2012-07-20 09:20:37 +0000

    [misc] UniqueIDService was refactored;

--- trunk/core/src/main/java/com/exadel/dinnerorders/service/UniqueIDService.java
+++ trunk/core/src/main/java/com/exadel/dinnerorders/service/UniqueIDService.java
@@ -1,7 +1,12 @@
 package com.exadel.dinnerorders.service;
-import com.exadel.dinnerorders.dao.BaseDAO;
+
+import com.exadel.dinnerorders.entity.SystemResource;
+import org.apache.log4j.Logger;
+
+import java.sql.*;
 
 public class UniqueIDService {
+    private static final Logger logger = Logger.getLogger(UniqueIDService.class);
     private static Long ID;
 
     synchronized public static Long getID() {
@@ -15,11 +20,49 @@ public class UniqueIDService {
     }
 
     private static void saveNewID() {
-        //Save new generated ID to DB;
+        Connection connection = createConnection();
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = connection.prepareStatement("UPDATE id_table SET id = ?;");
+            preparedStatement.setLong(1, ID);
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException sqlException) {
+            logger.error("UniqueIDService: can't save new used id");
+        }
     }
 
     private static void loadLastUsedID() {
-        //Load from DB last used ID
+        Connection connection = createConnection();
+        Statement statement;
+        try {
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM dinnerorders.id_table");
+            resultSet.next();
+            ID = resultSet.getLong(1);
+            statement.close();
+            connection.close();
+        } catch (SQLException sqlException) {
+            logger.error("UniqueIDService: can't load last used id");
+        }
     }
 
+    private static Connection createConnection() {
+        com.mysql.jdbc.Connection connection = null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://" + Configuration.getProperty(SystemResource.DATABASE_HOST)
+                    + ":" + Configuration.getProperty(SystemResource.DATABASE_PORT)
+                    + "/" + Configuration.getProperty(SystemResource.DATABASE_NAME);
+
+            String login = Configuration.getProperty(SystemResource.DATABASE_LOGIN);
+            String password = Configuration.getProperty(SystemResource.DATABASE_PASSWORD);
+            connection = (com.mysql.jdbc.Connection) DriverManager.getConnection(url, login, password);
+        }catch(ClassNotFoundException e){
+            logger.error("UniqueIDService: class has not been found.", e);
+        }catch(SQLException e){
+            logger.error("UniqueIDService: connection has failed.", e);
+        }
+        return connection;
+    }
 }
