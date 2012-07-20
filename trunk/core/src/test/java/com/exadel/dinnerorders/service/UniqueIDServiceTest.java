c2fb7de796b668a45e4146f8dfcf02738b48b147
snalivko93@gmail.com <snalivko93@gmail.com@c07ae454-4001-a9b2-41ff-d98365e2add6>
2012-07-20 13:17:33 +0000

    [misc] refactoring.

---
+++ trunk/core/src/test/java/com/exadel/dinnerorders/service/UniqueIDServiceTest.java
@@ -0,0 +1,13 @@
+package com.exadel.dinnerorders.service;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class UniqueIDServiceTest {
+    @Test
+    public void testGetID(){
+//        Long id1 = UniqueIDService.getID();
+//        Long id2 = UniqueIDService.getID();
+//        Assert.assertFalse(id1.equals(id2));
+    }
+}
