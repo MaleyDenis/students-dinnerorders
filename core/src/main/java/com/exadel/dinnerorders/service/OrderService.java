cabab96a0d6546e61330b4a2e0abb538ed93dfe8
fry1228 <fry1228@gmail.com>
2012-07-30 11:21:34 +0000

    [MISC] refactoring OrderService.

--- core/src/main/java/com/exadel/dinnerorders/service/OrderService.java
+++ core/src/main/java/com/exadel/dinnerorders/service/OrderService.java
@@ -2,14 +2,21 @@ package com.exadel.dinnerorders.service;
 
 import com.exadel.dinnerorders.dao.OrderDAO;
 import com.exadel.dinnerorders.entity.Order;
+import com.exadel.dinnerorders.exception.WorkflowException;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
 import com.google.common.collect.Ordering;
 
-
+import javax.annotation.Nullable;
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.Date;
 
 
 public class OrderService {
+
+    private static OrderDAO orderDAO = new OrderDAO();
+
     public static Comparator<Order> byDate = new Comparator<Order>() {
         public int compare(final Order p1, final Order p2) {
             return p1.getDateOrder().compareTo(p2.getDateOrder());
@@ -22,12 +29,35 @@ public class OrderService {
     }
 
     public static Collection<Order> getAllSortedOrders() {
-        OrderDAO orderDAO = new OrderDAO();
         Collection<Order> ordersSorted = orderDAO.loadAll();
         if (ordersSorted != null) {
             getSortedOrders(ordersSorted);
         }
         return ordersSorted;
+    }
+
+    private static Order findOrderByDate(final Date date){
+        Predicate<Order> predicate = new Predicate<Order>() {
+            public boolean apply(@Nullable Order  order) {
+                return order != null &&
+                        (order.getDateOrder().equals(date));
+            }
+        };
+        Collection<Order> result =  Collections2.filter(orderDAO.loadAll(), predicate);
+
+        if (result.size() > 1) {
+            throw new WorkflowException();
+        }
+
+        if (result.isEmpty()) {
+            return null;
+        }
+
+        return result.iterator().next();
+    }
+
+    private static boolean deleteOrder(Order order){
+        return orderDAO.delete(order);
     }
 }
 
