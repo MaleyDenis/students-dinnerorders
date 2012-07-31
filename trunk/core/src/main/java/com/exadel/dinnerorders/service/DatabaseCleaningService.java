76909434c4bfa90a126206da077ef7d789f3f596
snalivko93@gmail.com <snalivko93@gmail.com@c07ae454-4001-a9b2-41ff-d98365e2add6>
2012-07-31 10:09:27 +0000

    [Issue 40] fixing DatabaseCleaningService and according test

--- trunk/core/src/main/java/com/exadel/dinnerorders/service/DatabaseCleaningService.java
+++ trunk/core/src/main/java/com/exadel/dinnerorders/service/DatabaseCleaningService.java
@@ -9,16 +9,17 @@ import java.util.Collection;
 import java.util.concurrent.*;
 
 public class DatabaseCleaningService {
+    public static final int NUMBER_OF_THREADS = 1;
     private long startDelay;
     private long intervalDelay;
     private TimeUnit unit;
     private long elapsedTime;
     private ScheduledExecutorService service;
-    private int initialDelay;
     private ClearTableTask clearTableTask;
-    private Boolean executionResult = false;
+    private final Phaser phaser;
 
     public DatabaseCleaningService() {
+        phaser = new Phaser(NUMBER_OF_THREADS);
         this.startDelay = Long.parseLong(Configuration.getProperty(SystemResource.DELETION_SERVICE_START_DELAY));
         this.intervalDelay = Long.parseLong(Configuration.getProperty(SystemResource.DELETION_SERVICE_INTERVAL_DELAY));
         unit = TimeUnit.valueOf(Configuration.getProperty(SystemResource.TIME_UNIT));
@@ -27,12 +28,8 @@ public class DatabaseCleaningService {
     }
 
     public void start() {
-        clearTableTask = new ClearTableTask();
+        clearTableTask = new ClearTableTask(phaser);
         service.scheduleAtFixedRate(clearTableTask, startDelay, intervalDelay, unit);
-    }
-
-    public int getInitialDelay() {
-        return initialDelay;
     }
 
     public void stop() {
@@ -68,18 +65,26 @@ public class DatabaseCleaningService {
     public void setIntervalDelay(long intervalDelay) {
         this.intervalDelay = intervalDelay;
         service.shutdownNow();
-        clearTableTask = new ClearTableTask();
+        clearTableTask = new ClearTableTask(phaser);
         service.scheduleAtFixedRate(clearTableTask, startDelay, intervalDelay, unit);
     }
 
-    public boolean isAlreadyOnceCleared() {
-        return executionResult;
+    public int waitCertainLaunch(int nLaunch) {
+        return phaser.awaitAdvance(nLaunch);
     }
 
     private class ClearTableTask implements Runnable {
+        private final Phaser phaser;
+
+        public ClearTableTask(Phaser phaser) {
+            this.phaser = phaser;
+        }
+
         @Override
         public void run() {
-            executionResult = clearOrderTable() && clearMenuTable();
+            clearOrderTable();
+            clearMenuTable();
+            phaser.arrive();
         }
     }
 }
