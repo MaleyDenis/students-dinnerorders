2b093830bffcddd228f13ae13ddaef02f1fae4a5
123dudar <123dudar@gmail.com>
2012-07-31 14:18:06 +0000

    [MISC] new package: cache. New classes in package cache: Cache(abstract), MenuCache, MenuFactory. Fixed MenuDAO.

---
+++ core/src/main/java/com/exadel/dinnerorders/cache/MenuFactory.java
@@ -0,0 +1,26 @@
+package com.exadel.dinnerorders.cache;
+
+import com.exadel.dinnerorders.dao.MenuDAO;
+import com.exadel.dinnerorders.entity.Menu;
+import net.sf.ashkay.CreationException;
+import net.sf.ashkay.ObjectFactory;
+
+/**
+ * User: Василий Силин
+ * Date: 31.7.12
+ */
+class MenuFactory implements ObjectFactory {
+    private MenuDAO menuDAO = new MenuDAO();
+
+    @Override
+    public Object createObjectFor(Object key, Object data) throws CreationException {
+        if (key.getClass() != Long.class) {
+            throw new CreationException("ID is not type Long. Enter correct key.");
+        }
+        Menu menu = menuDAO.load((Long)key);
+        if (menu == null) {
+            throw new CreationException("No menu with this ID.");
+        }
+        return menu;
+    }
+}
