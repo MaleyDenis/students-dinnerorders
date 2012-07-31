d49487ea15240fbac90cde98ad2b6ff91153795d
123dudar@gmail.com <123dudar@gmail.com@c07ae454-4001-a9b2-41ff-d98365e2add6>
2012-07-31 10:47:23 +0000

    [MISC] fixed MenuService

--- trunk/core/src/main/java/com/exadel/dinnerorders/service/MenuService.java
+++ trunk/core/src/main/java/com/exadel/dinnerorders/service/MenuService.java
@@ -4,85 +4,65 @@ import com.exadel.dinnerorders.dao.MenuDAO;
 import com.exadel.dinnerorders.dao.MenuItemDAO;
 import com.exadel.dinnerorders.entity.Menu;
 import com.exadel.dinnerorders.entity.MenuItem;
-import com.exadel.dinnerorders.entity.Weekday;
+import com.exadel.dinnerorders.exception.WorkflowException;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Collections2;
+
 import org.apache.log4j.Logger;
 
 import javax.annotation.Nullable;
-import java.sql.Timestamp;
-import java.util.ArrayList;
+
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
+
+/**
+ * User: Василий Силин
+ * Date: 18.7.12
+ */
 
 public class MenuService {
     private Logger logger = Logger.getLogger(MenuService.class);
 
     private static MenuDAO menuDAO = new MenuDAO();
+    private static MenuItemDAO menuItemDAO = new MenuItemDAO();
 
-    public static Collection<Menu> findMenuForNextWeek() {
-        final Timestamp nextMondayDate = DateUtils.getNextMondayTime();
-        long availableTime = nextMondayDate.getTime();
-        return findMenuByDate(new Timestamp(availableTime));
+    public static Menu findMenuForNextWeek() {
+
+        return null;
     }
 
-    public static Collection<Menu> findMenuForCurrentWeek(){
-        final Timestamp currentMondayDate = DateUtils.getCurrentMondayTime();
-        long availableTime = currentMondayDate.getTime();
-        return findMenuByDate(new Timestamp(availableTime));
+    public static Menu findMenuForCurrentWeek(){
+        final Date date = new Date();
+        return findMenuByDate(date);
     }
 
-    public static Collection<Menu> findMenuByDate(final Date date){
+    public static Menu findMenuByDate(final Date date){
         Predicate<Menu> predicate = new Predicate<Menu>() {
             public boolean apply(@Nullable Menu o) {
-                return o != null &&
-                        (o.getDateStart().before(date) || o.getDateStart().equals(date)) &&
-                        (o.getDateEnd().after(date) || o.getDateEnd().equals(date));
+                return o != null && o.getDateStart().before(date) && o.getDateEnd().after(date);
             }
         };
+        MenuDAO menuDAO = new MenuDAO();
         Collection<Menu> result =  Collections2.filter(menuDAO.loadAll(), predicate);
 
-//        if (result.size() > 1) {
-//            throw new WorkflowException();
-//        }
+        if (result.size() > 1) {
+            throw new WorkflowException();
+        }
 
         if (result.isEmpty()) {
             return null;
         }
 
-        return result;
+        return result.iterator().next();
     }
 
     public static boolean save(Menu newMenu){
-        MenuDAO menuDAO = new MenuDAO();
-        MenuItemDAO menuItemDAO = new MenuItemDAO();
         for(List<MenuItem> items : newMenu.getItems().values()){
             for(MenuItem item : items){
                 menuItemDAO.create(item);
             }
         }
-        menuDAO.create(newMenu);
-        return true;
-    }
-
-    public static boolean update(Menu newMenu){
-        MenuDAO menuDAO = new MenuDAO();
-        return menuDAO.update(newMenu);
-    }
-
-    public static boolean delete(Menu menu){
-        MenuDAO menuDAO = new MenuDAO();
-        List<MenuItem> menuItems = new ArrayList<MenuItem>();
-        for (int i = 1; i <= DateUtils.DEFAULT_WORK_DAYS; i++) {
-            if (menu.getItems().get(Weekday.getWeekday(i)) != null) {
-                menuItems.addAll(menu.getItems().get(Weekday.getWeekday(i)));
-            }
-        }
-        MenuItemDAO menuItemDAO = new MenuItemDAO();
-        for (MenuItem menuItem : menuItems) {
-            menuItemDAO.delete(menuItem);
-        }
-        return menuDAO.delete(menu);
+        return (menuDAO.create(newMenu) ? true : false);
     }
 }
